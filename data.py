Label mapping and 2-class training supported. EER calculation bug fixed.

data.py
@@ -1,7 +1,9 @@
 import lightning as L
 import torch
+import torchaudio
 from torch.utils.data import Dataset, DataLoader
 from datasets import load_from_disk
+from pathlib import Path
 
 from utils import *
 
@@ -47,6 +49,7 @@ class EchoFakeModule(L.LightningDataModule):
         sample_rate=16000,
         num_workers=4,
         max_len=64000,
+        label_mapping=None,
     ):
         super().__init__()
         self.dataset_path = dataset_path
@@ -54,14 +57,25 @@ class EchoFakeModule(L.LightningDataModule):
         self.sample_rate = sample_rate
         self.num_workers = num_workers
         self.max_len = max_len
+        self.label_mapping = label_mapping
 
     def setup(self, stage=None):
         dataset = load_from_disk(self.dataset_path)
 
-        self.train_dataset = HuggingFaceAudioDataset(dataset["train"], self.max_len)
-        self.val_dataset = HuggingFaceAudioDataset(dataset["dev"], self.max_len)
+        self.train_dataset = HuggingFaceAudioDataset(
+            dataset["train"], self.max_len, label_mapping=self.label_mapping
+        )
+        self.val_dataset = HuggingFaceAudioDataset(
+            dataset["dev"],
+            self.max_len,
+            pad_mode="normal",
+            label_mapping=self.label_mapping,
+        )
         self.test_dataset = HuggingFaceAudioDataset(
-            dataset["closed_set_eval"], self.max_len
+            dataset["closed_set_eval"],
+            self.max_len,
+            pad_mode="normal",
+            label_mapping=self.label_mapping,
         )
 
     def train_dataloader(self):
@@ -87,3 +101,106 @@ class EchoFakeModule(L.LightningDataModule):
             shuffle=False,
             num_workers=self.num_workers,
         )
+
+
+class ASVspoof2019Dataset(Dataset):
+    def __init__(
+        self,
+        base_dir,
+        protocol_dir,
+        pad_mode="random",
+        max_len=64000,
+        config=None,
+    ):
+        self.list_IDs = []
+        self.labels = []
+        self.base_dir = base_dir
+        self.protocol_dir = protocol_dir
+        self.config = config
+        with open(self.protocol_dir, "r") as f:
+            lines = f.readlines()
+            for line in lines:
+                # example line: LA_0098 LA_T_9779812 - - bonafide
+                _, key, _, _, label = line.strip().split(" ")
+                self.list_IDs.append(key)
+
+                if label == "bonafide":
+                    self.labels.append(1)
+                else:
+                    self.labels.append(0)
+
+        assert pad_mode in ["normal", "random"]
+        if pad_mode == "random":
+            self.pad = pad_random
+        else:
+            self.pad = pad
+
+        self.max_len = max_len
+
+    def __len__(self):
+        return len(self.list_IDs)
+
+    def __getitem__(self, index):
+        key = self.list_IDs[index]
+        x = torchaudio.load(str(self.base_dir / f"flac/{key}.flac"))[0].squeeze(0)
+        x = torch.Tensor(self.pad(x, self.max_len))
+        y = torch.LongTensor([1]) if self.labels[index] == 1 else torch.LongTensor([0])
+        y = y.squeeze()
+        return x, y
+
+
+class ASVspoof2019(L.LightningDataModule):
+    def __init__(self, dataset_path, track="LA", max_len=64000, **dataloaderArgs):
+        """
+        ASVspoof 2019 LA datamodule for training
+
+        """
+        super().__init__()
+        self.max_len = max_len
+        self.track = track
+        self.base_dir = Path(dataset_path) / track
+        assert track in ["LA", "PA"]
+        self.protocol_dir = self.base_dir / f"ASVspoof2019_{track}_cm_protocols"
+        self.train_cm = self.protocol_dir / f"ASVspoof2019.{track}.cm.train.trn.txt"
+        self.dev_cm = self.protocol_dir / f"ASVspoof2019.{track}.cm.dev.trl.txt"
+        self.eval_cm = self.protocol_dir / f"ASVspoof2019.{track}.cm.eval.trl.txt"
+        self.dev_asv_scores = (
+            self.base_dir
+            / f"ASVspoof2019_{track}_asv_scores"
+            / f"ASVspoof2019.{track}.asv.dev.gi.trl.scores.txt"
+        )
+        self.eval_asv_scores = (
+            self.base_dir
+            / f"ASVspoof2019_{track}_asv_scores"
+            / f"ASVspoof2019.{track}.asv.eval.gi.trl.scores.txt"
+        )
+        self.dataloaderArgs = dataloaderArgs
+
+    def setup(self, stage: str):
+        # read ASVSpoof 2019 LA protocol
+        dataset_dir = self.base_dir / f"ASVspoof2019_{self.track}_train"
+        self.train_data = ASVspoof2019Dataset(
+            dataset_dir,
+            self.train_cm,
+            pad_mode="random",
+            max_len=self.max_len,
+        )
+
+        dataset_dir = self.base_dir / f"ASVspoof2019_{self.track}_dev"
+        self.val_data = ASVspoof2019Dataset(
+            dataset_dir, self.dev_cm, pad_mode="normal", max_len=self.max_len
+        )
+
+        dataset_dir = self.base_dir / f"ASVspoof2019_{self.track}_eval"
+        self.test_data = ASVspoof2019Dataset(
+            dataset_dir, self.eval_cm, pad_mode="normal", max_len=self.max_len
+        )
+
+    def train_dataloader(self):
+        return DataLoader(self.train_data, shuffle=True, **self.dataloaderArgs)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_data, shuffle=False, **self.dataloaderArgs)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_data, shuffle=False, **self.dataloaderArgs)

train.py
@@ -3,20 +3,86 @@ import torch
 
 from models import RawNet2, AASIST
 from utils import *
+from data import EchoFakeModule
 
 
-def get_trainer(model_name):
-    if model_name.lower() == "rawnet2":
-        return RawNet2Trainer
-    elif model_name.lower() == "aasist":
-        return AASISTTrainer
+class BaseTrainer(L.LightningModule):
+    def on_validation_epoch_start(self):
+        self.labels = []
+        self.preds = []
+        self.logits = []
+        self.target_scores = []
+        self.nontarget_scores = []
+
+    def validation_step(self, val_batch, batch_idx):
+        x, y = val_batch
+        emb, logits = self.model(x)
+
+        preds = torch.argmax(logits, dim=1)
+        self.labels.extend(y.tolist())
+        self.preds.extend(preds.tolist())
+
+        if self.config["num_classes"] == 2:
+            self.logits.extend(logits[:, 1].tolist())
+            for i in range(x.size(0)):
+                if y[i] == 1:
+                    self.target_scores.append(logits[i, 1].item())
+                else:
+                    self.nontarget_scores.append(logits[i, 1].item())
+
+    def on_validation_epoch_end(self):
+        if self.config["num_classes"] == 2:
+            eer, threshold = compute_eer(self.target_scores, self.nontarget_scores)
+            self.log("val/eer", eer, sync_dist=True)
+            self.preds = [1 if score >= threshold else 0 for score in self.logits]
+
+        precision, recall, f1 = compute_f1(self.labels, self.preds)
+
+        self.log("val/precision", precision, sync_dist=True)
+        self.log("val/recall", recall, sync_dist=True)
+        self.log("val/f1", f1, sync_dist=True)
+
+    def on_test_epoch_start(self):
+        self.labels = []
+        self.preds = []
+        self.logits = []
+        self.target_scores = []
+        self.nontarget_scores = []
+
+    def test_step(self, test_batch, batch_idx):
+        x, y = test_batch
+        emb, logits = self.model(x)
+
+        preds = torch.argmax(logits, dim=1)
+        self.labels.extend(y.tolist())
+        self.preds.extend(preds.tolist())
+        if self.config["num_classes"] == 2:
+            self.logits.extend(logits[:, 1].tolist())
+            for i in range(x.size(0)):
+                if y[i] == 1:
+                    self.target_scores.append(logits[i, 1].item())
+                else:
+                    self.nontarget_scores.append(logits[i, 1].item())
+
+    def on_test_epoch_end(self):
+        if self.config["num_classes"] == 2:
+            eer, threshold = compute_eer(self.target_scores, self.nontarget_scores)
+            self.log("test/eer", eer, sync_dist=True)
+            self.preds = [1 if score >= threshold else 0 for score in self.logits]
+
+        precision, recall, f1 = compute_f1(self.labels, self.preds)
+
+        self.log("test/precision", precision, sync_dist=True)
+        self.log("test/recall", recall, sync_dist=True)
+        self.log("test/f1", f1, sync_dist=True)
 
 
-class RawNet2Trainer(L.LightningModule):
+class RawNet2Trainer(BaseTrainer):
     def __init__(self, config):
         super().__init__()
         self.config = config
-        self.model = RawNet2(nb_classes=4)
+        self.model = RawNet2(num_classes=config["num_classes"])
+        # weight = torch.FloatTensor([0.1, 0.9])
         self.ce_loss = torch.nn.CrossEntropyLoss()
 
     def configure_optimizers(self):
@@ -37,53 +103,13 @@ class RawNet2Trainer(L.LightningModule):
 
         return loss
 
-    def on_validation_epoch_start(self):
-        self.labels = []
-        self.preds = []
 
-    def validation_step(self, val_batch, batch_idx):
-        x, y = val_batch
-        emb, logits = self.model(x)
-
-        preds = torch.argmax(logits, dim=1)
-        self.labels.extend(y.tolist())
-        self.preds.extend(preds.tolist())
-
-    def on_validation_epoch_end(self):
-        precision, recall, f1 = compute_f1(self.labels, self.preds)
-        eer = compute_eer(self.preds, self.labels)
-        self.log("val/precision", precision, sync_dist=True)
-        self.log("val/recall", recall, sync_dist=True)
-        self.log("val/f1", f1, sync_dist=True)
-        self.log("val/eer", eer, sync_dist=True)
-
-    def on_test_epoch_start(self):
-        self.labels = []
-        self.preds = []
-
-    def test_step(self, test_batch, batch_idx):
-        x, y = test_batch
-        emb, logits = self.model(x)
-
-        preds = torch.argmax(logits, dim=1)
-        self.labels.extend(y.tolist())
-        self.preds.extend(preds.tolist())
-
-    def on_test_epoch_end(self):
-        precision, recall, f1 = compute_f1(self.labels, self.preds)
-        eer = compute_eer(self.preds, self.labels)
-        self.log("test/precision", precision, sync_dist=True)
-        self.log("test/recall", recall, sync_dist=True)
-        self.log("test/f1", f1, sync_dist=True)
-        self.log("test/eer", eer, sync_dist=True)
-
-
-class AASISTTrainer(L.LightningModule):
+class AASISTTrainer(BaseTrainer):
     def __init__(self, config):
         super().__init__()
         self.config = config
 
-        self.model = AASIST(d_args=config["d_args"], num_classes=4)
+        self.model = AASIST(d_args=config["d_args"], num_classes=config["num_classes"])
         self.ce_loss = torch.nn.CrossEntropyLoss()
 
     def configure_optimizers(self):
@@ -115,43 +141,3 @@ class AASISTTrainer(L.LightningModule):
         self.log("train/loss", loss)
 
         return loss
-
-    def on_validation_epoch_start(self):
-        self.labels = []
-        self.preds = []
-
-    def validation_step(self, val_batch, batch_idx):
-        x, y = val_batch
-        emb, logits = self.model(x)
-
-        preds = torch.argmax(logits, dim=1)
-        self.labels.extend(y.tolist())
-        self.preds.extend(preds.tolist())
-
-    def on_validation_epoch_end(self):
-        precision, recall, f1 = compute_f1(self.labels, self.preds)
-        eer = compute_eer(self.preds, self.labels)
-        self.log("val/precision", precision, sync_dist=True)
-        self.log("val/recall", recall, sync_dist=True)
-        self.log("val/f1", f1, sync_dist=True)
-        self.log("val/eer", eer, sync_dist=True)
-
-    def on_test_epoch_start(self):
-        self.labels = []
-        self.preds = []
-
-    def test_step(self, test_batch, batch_idx):
-        x, y = test_batch
-        emb, logits = self.model(x)
-
-        preds = torch.argmax(logits, dim=1)
-        self.labels.extend(y.tolist())
-        self.preds.extend(preds.tolist())
-
-    def on_test_epoch_end(self):
-        precision, recall, f1 = compute_f1(self.labels, self.preds)
-        eer = compute_eer(self.preds, self.labels)
-        self.log("test/precision", precision, sync_dist=True)
-        self.log("test/recall", recall, sync_dist=True)
-        self.log("test/f1", f1, sync_dist=True)
-        self.log("test/eer", eer, sync_dist=True)

utils.py
@@ -1,6 +1,11 @@
 import numpy as np
 import torch
-from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.metrics import (
+    precision_score,
+    recall_score,
+    f1_score,
+    precision_recall_curve,
+)
 
 
 def pad(x, max_len=64000):
@@ -54,7 +59,7 @@ def compute_f1(labels, preds):
     precision = precision_score(labels, preds, average="macro")
     recall = recall_score(labels, preds, average="macro")
     f1 = f1_score(labels, preds, average="macro")
-    print(f"Precision: {precision:.4f}, Recall: {recall:.4f}, F1: {f1:.4f}\n")
+    # print(f"Precision: {precision:.4f}, Recall: {recall:.4f}, F1: {f1:.4f}")
     return precision, recall, f1
 
 
@@ -95,19 +100,30 @@ def compute_det_curve(target_scores, nontarget_scores):
     return frr, far, thresholds
 
 
-def compute_eer(pred, label):
-    """Returns equal error rate (EER) and the corresponding threshold."""
-    if isinstance(pred, torch.Tensor):
-        pred, label = pred.cpu().numpy(), label.cpu().numpy()
+# def compute_eer(pred, label):
+#     """Returns equal error rate (EER) and the corresponding threshold."""
+#     if isinstance(pred, torch.Tensor):
+#         pred, label = pred.cpu().numpy(), label.cpu().numpy()
 
-    pred, label = np.array(pred).flatten(), np.array(label).flatten()
-    target_scores = pred[label == 1.0]
-    nontarget_scores = pred[label == 0.0]
+#     pred, label = np.array(pred).flatten(), np.array(label).flatten()
+#     target_scores = pred[label == 1.0]
+#     nontarget_scores = pred[label == 0.0]
+#     frr, far, thresholds = compute_det_curve(target_scores, nontarget_scores)
+#     abs_diffs = np.abs(frr - far)
+#     min_index = np.argmin(abs_diffs)
+#     eer = np.mean((frr[min_index], far[min_index]))
+#     return eer  # , thresholds[min_index]
+
+
+def compute_eer(target_scores, nontarget_scores):
+    """Returns equal error rate (EER) and the corresponding threshold."""
+    target_scores = np.array(target_scores).flatten()
+    nontarget_scores = np.array(nontarget_scores).flatten()
     frr, far, thresholds = compute_det_curve(target_scores, nontarget_scores)
     abs_diffs = np.abs(frr - far)
     min_index = np.argmin(abs_diffs)
     eer = np.mean((frr[min_index], far[min_index]))
-    return eer  # , thresholds[min_index]
+    return eer, thresholds[min_index]
 
 
 def cosine_annealing(step, total_steps, lr_max, lr_min):